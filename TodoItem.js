1f45c347a054874990985dd5e50e28aa8aa8b492
KAYLEIGH <kayleightuppen@gmail.com>
2021-04-08 09:06:42 +0100

    Add files via upload

---
+++ TodoItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class TodoItem extends React.Component {
+
+    render() {
+
+        const { todo } = this.props;
+
+        return(
+        <div className={'todoItem' + (this.todo.completed ? ' completed' : '')}onClick={this.toggleTodo}>{todo.text}</div>);
+    }
+
+    toggleTodo = () => {
+        this.props.updateTodoFn(this.props.todo);
+    }
+}
+
+export default TodoItem;
